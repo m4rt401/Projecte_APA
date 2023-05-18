db8a15630a37acb9954f6e930d255fb1de04b820
m4rt401 <marta.enrich.garcia@estudiantat.upc.edu>
2023-05-18 16:30:53 +0200

    primero

--- frontend.py
+++ frontend.py
@@ -1,1 +1,22 @@
+import tkinter as tk
+from tkinter import filedialog
 
+def open_file_dialog():
+    file_path = filedialog.askopenfilename()
+    selected_file_label.config(text="Archivo seleccionado: " + file_path)
+    # Realizar el procesamiento adicional aquí
+
+root = tk.Tk()
+root.title("Front-end")
+
+def create_widgets():
+    browse_button = tk.Button(root, text="Browse", command=open_file_dialog)
+    browse_button.pack(pady=10)
+
+    global selected_file_label
+    selected_file_label = tk.Label(root, text="Archivo seleccionado: ")
+    selected_file_label.pack()
+
+create_widgets()
+
+root.mainloop()
