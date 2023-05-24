3cc443d9129134c31cbfcb1afd75a37bdcc3cfd4
Rafael E Moncayo Palate <rafael.eduardo.moncayo@estudiantat.upc.edu>
2023-05-24 12:13:38 +0200

    Backend + intento front end

--- frontend.py
+++ frontend.py
@@ -1,3 +1,4 @@
+'''
 import tkinter as tk
 from tkinter import filedialog
 
@@ -20,3 +21,89 @@ def create_widgets():
 create_widgets()
 
 root.mainloop()
+
+
+from PIL import Image, ImageTk
+import tkinter as tk
+
+
+class FaderApp(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.geometry("200x300")
+        self.title("Fader App")
+
+        self.value = tk.IntVar()
+        self.value.set(0)
+
+        self.canvas = tk.Canvas(self, width=50, height=250, bg="white")
+        self.canvas.pack()
+
+        # Cargar imágenes del fader y la perilla
+        fader_image = Image.open("img/fader_image.png")
+        knob_image = Image.open("img/knob_image.png")
+
+        # Escalar las imágenes para que se ajusten al tamaño del canvas
+        fader_image = fader_image.resize((50, 250))
+        knob_image = knob_image.resize((30, 30))
+
+        self.fader = self.canvas.create_image(25, 125, image=self._pil_to_imageTk(fader_image))
+        self.knob = self.canvas.create_image(25, 225, image=self._pil_to_imageTk(knob_image))
+
+        self.canvas.bind("<B1-Motion>", self.move_knob)
+        self.canvas.bind("<Button-1>", self.move_knob)
+        
+        self.label = tk.Label(self, textvariable=self.value)
+        self.label.pack()
+
+    def move_knob(self, event):
+        y = event.y
+        if y < 25:
+            y = 25
+        elif y > 225:
+            y = 225
+        self.canvas.coords(self.knob, 25, y)
+        self.value.set(int(100 - (y-25) * 100 / 200))
+
+    def _pil_to_imageTk(self, image):
+        return ImageTk.PhotoImage(image)
+
+if __name__ == "__main__":
+    app = FaderApp()
+    app.mainloop()
+''' 
+import tkinter as tk
+
+class FaderApp(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.geometry("200x300")
+        self.title("Fader App")
+
+        self.value = tk.IntVar(value=100)
+        self.value.set(0)
+
+        self.canvas = tk.Canvas(self, width=50, height=250, bg="white")
+        self.canvas.pack()
+
+        self.fader = self.canvas.create_rectangle(20, 20, 30, 230, fill="black")
+        self.knob = self.canvas.create_oval(10, 225, 40, 255, fill="red")
+
+        self.canvas.bind("<B1-Motion>", self.move_knob)
+        self.canvas.bind("<Button-1>", self.move_knob)
+        
+        self.label = tk.Label(self, textvariable=self.value)
+        self.label.pack()
+
+    def move_knob(self, event):
+        y = event.y
+        if y < 25:
+            y = 25
+        elif y > 225:
+            y = 225
+        self.canvas.coords(self.knob, 10, y-5, 40, y+5)
+        self.value.set(int(100 - (y-25) * 100 / 200))
+
+if __name__ == "__main__":
+    app = FaderApp()
+    app.mainloop()
