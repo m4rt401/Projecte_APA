ef9ea11ba151cffb2623a0064a176fad5e63eb67
m4rt401 <marta.enrich.garcia@estudiantat.upc.edu>
2023-05-25 16:56:30 +0200

    inicio de juntasion

--- main.py
+++ main.py
@@ -2,7 +2,59 @@ import tkinter as tk
 from tkinter import filedialog
 from PIL import Image, ImageTk
 import pygame
+import soundfile as sf
+import sounddevice as sd
+import numpy as np
+import math
+import wave as wv
 
+# back
+def lectura_audio(Ficheroaudio):
+    '''
+    Lee el fichero de audio introducido y retorna los datos, el sample rate y la información de la cabecera del archivo sea del tipo que sea.
+    En el caso de que sea un archivo no valido saltará una excepción que nos llevarà a una ventana de alerta.
+    '''
+    try:
+        data, samplerate = sf.read(Ficheroaudio)
+        info = sf.info(Ficheroaudio)
+        # saca la información de la cabecera con la libreria SF para después 
+        # codificar el archivo de salida con la misma información con la que 
+        # entra sea cual sea el tipo de archivo de entrada
+    except (TypeError, sf.SoundFileError) as e:
+        # mensaje de alerta por introducir un fichero incorrecto
+        print("Error al leer el archivo:", str(e))
+    
+    return data,samplerate,info
+
+def downsampler(data, samplerate, info, fader):
+    if 100 > fader & fader>0:
+        factor_degradacion = (fader/100)
+    elif fader == 0:
+        factor_degradacion = 1
+    else: 
+        raise Exception("Valor fuera de valores permitidos") from None 
+    
+    print(factor_degradacion)
+    samplerate_nuevo = int(samplerate*factor_degradacion/2)
+    factor_downsampling = math.ceil(samplerate/samplerate_nuevo)
+    print(factor_downsampling)
+    downsampled_data = np.zeros_like(data)
+    downsampled_data[::factor_downsampling] = data[::factor_downsampling]
+
+    temp_file = "temp_audio.wav"
+    sf.write(temp_file, downsampled_data, samplerate, subtype=info.subtype, format='WAV')
+    data, _ = sf.read(temp_file, dtype='float32')
+    sd.play(data)
+
+    return downsampled_data, samplerate, info
+
+def escritura_wave(data, samplerate, info):
+    # Crear el objeto SoundFile como un archivo wav con la misma cabecera que info
+    out_file = "Downsampled_out.wav"
+    sf.write(out_file, data, samplerate, subtype=info.subtype, format='WAV')
+    
+
+# front
 class FaderApp(tk.Tk):
     def __init__(self):
         super().__init__()
