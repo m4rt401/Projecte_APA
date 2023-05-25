cb82ac0e7833d6018fab601effc815e7eae2eb84
Marta Enrich Garcia <79660030+m4rt401@users.noreply.github.com>
2023-05-25 16:12:26 +0200

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -7,27 +7,32 @@ class FaderApp(tk.Tk):
         super().__init__()
         self.geometry("500x500")
         self.title("Fader App")
-        self.value = tk.IntVar(value=100)
-        self.value.set(0)
+        self.value = tk.IntVar(value=0)
 
         # Cargar la imagen de fondo
-        self.image = ImageTk.PhotoImage(file="DOWNSAMPLING.png")
+        self.image = ImageTk.PhotoImage(file="Projecte_APA/DOWNSAMPLING.png")
 
         # Crear un widget Label y establecer la imagen de fondo
         background_label = tk.Label(self, image=self.image)
         background_label.place(x=0, y=0, relwidth=1, relheight=1)
 
-        self.canvas = tk.Canvas(self, width=10, height=230, bg="black", highlightthickness=0)
-        self.canvas.place(x=127.5, y=142)  # Posición deseada del fader
+        self.canvas = tk.Canvas(self, width=10, height=225, bg="black", highlightthickness=0)
+        self.canvas.place(x=120, y=126)  # Posición deseada del fader
 
-        self.fader = self.canvas.create_rectangle(0, 0, 20, 230, fill="white")
+        self.fader = self.canvas.create_rectangle(0, 230, 280, 0, fill="white")
         self.knob = self.canvas.create_oval(-5, -5, 15, 15, fill="red")
 
         self.canvas.bind("<B1-Motion>", self.move_knob)
         self.canvas.bind("<Button-1>", self.move_knob)
 
         self.label = tk.Label(self, textvariable=self.value)
-        self.label.place(x=240, y=400)  # Posición deseada de la etiqueta
+        self.label.place(x=114, y=355)  # Posición deseada de la etiqueta
+
+        self.selected_file_label = tk.Label(self, text="Archivo seleccionado: ")
+        self.selected_file_label.place(x=20, y=470)  # Posición deseada de la etiqueta de ruta seleccionada
+
+        self.browse_button = tk.Button(self, text="Browse", command=self.open_file_dialog)
+        self.browse_button.place(x=450, y=470)  # Posición deseada del botón de navegación
 
     def move_knob(self, event):
         y = event.y
@@ -35,9 +40,14 @@ class FaderApp(tk.Tk):
             y = 25
         elif y > 225:
             y = 225
-        self.canvas.coords(self.fader, 0, 0, 20, 230)  # Ajustar coordenadas del rectángulo
+        self.canvas.coords(self.fader, 0, y, 300, 230)  # Ajustar coordenadas del rectángulo
         self.canvas.coords(self.knob, -5, y-5, 15, y+5)
-        self.value.set(int(100 - (y-25) * 100 / 200))
+        self.value.set(int((y-25) * 100 / 200))
+
+    def open_file_dialog(self):
+        file_path = filedialog.askopenfilename()
+        if file_path:
+            self.selected_file_label.config(text="Archivo seleccionado: " + file_path)
 
 if __name__ == "__main__":
     app = FaderApp()
